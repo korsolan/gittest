02da160bc88795ab3509191acae6c285270a1585
korsolan <korsolan@gmail.com>
2022-12-09 10:14:53 +0300

    fix bug in

--- gh.py
+++ gh.py
@@ -9,7 +9,7 @@ def get_count_in_range(list, num_1, num_2):
     for i in range(len(list)):
         if num_1 <= list[i] <= num_2:
             count += 1
-            return count
+    return count
 def unique_temp(list_temp):
     new_list_temp = list(set(list_temp))
     return new_list_temp
